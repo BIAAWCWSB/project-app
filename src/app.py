0852256794bcb950358ef876e81fc2d90084aa3b
PawelTrzyna <91832822+PawelTrzyna@users.noreply.github.com>
2023-04-22 05:56:27 +0200

    Create app.py

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
+    return render_template('hello.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
