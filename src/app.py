3ea9de4d0f0c99c1b77bc0bb50e99ae81a6e9027
PawelTrzyna <91832822+PawelTrzyna@users.noreply.github.com>
2023-04-22 06:01:08 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,14 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return "Hello, World!"
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
         name = request.form['name']
-        return f'Hello, {name}!'
-    return render_template('hello.html')
+        return f"Hello, {name}!"
+    return render_template('hello_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
